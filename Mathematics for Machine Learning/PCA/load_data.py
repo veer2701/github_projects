07b26a96f64eed252f76f1a4b46c51dfcac6add1
Veer Pal Singh <veer.vsps@gmail.com>
2022-01-08 21:22:05 +0530

    Updates

---
+++ Mathematics for Machine Learning/PCA/load_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def load_mnist():
+    mnist_path = "mnist-original.mat"
+    from scipy.io import loadmat
+    mnist_raw = loadmat(mnist_path)
+    mnist = {
+        "data": mnist_raw["data"].T,
+        "target": mnist_raw["label"][0],
+        "COL_NAMES": ["label", "data"],
+        "DESCR": "mldata.org dataset: mnist-original",
+    }
+    return mnist
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+    x = np.array([3, 1, 2])
+    print(np.argsort(x))
+    print(x[np.argsort(x)])
+
